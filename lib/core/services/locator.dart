96ac70db4fa41eb77be0100d6b8e5db0120cf54f
Vitor Ferreira Iassia <iassiavitor@gmail.com>
2021-06-09 15:35:30 -0300

    configurando package DIO, para as chamadas de api

--- lib/core/services/locator.dart
+++ lib/core/services/locator.dart
@@ -1,9 +1,10 @@
+import 'package:interview_gigaservice_flutter/core/services/api.dart';
 import 'package:interview_gigaservice_flutter/pages/home/home_view_model.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerFactory(() => HomeViewModel());
-  // locator.registerLazySingleton(() => Api());
+  locator.registerLazySingleton(() => Api());
   // locator.registerLazySingleton(() => StorageService());
 }
